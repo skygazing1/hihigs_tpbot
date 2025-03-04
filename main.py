1d528edad2b4190c723c1f8f99a23b508dcf7ea4
skygazing1 <8120007@gmail.com>
2025-03-04 20:49:14 +0300

    Добавлены файлы .env, .ignore и исправлена структура кода

---
+++ main.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
+from dotenv import load_dotenv
+import asyncio
+import os
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def process_start_command(message: Message):
+    await message.answer("Привет!")
+
+@dp.message()
+async def echo_message(message: Message):
+    await message.answer(message.text)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
