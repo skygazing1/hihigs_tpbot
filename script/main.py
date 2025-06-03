0f687484581e00b9f5b1ace83965badf318a3512
Degtyarev Egor <egoordd@MacBook-Pro.local>
2025-06-03 20:18:51 +0300

    Рефакторинг: перемещена инициализация БД в script.db и добавлена переменная DATABASE_URL

--- script/main.py
+++ script/main.py
@@ -9,6 +9,16 @@ sys.path.insert(0, str(project_root))
 
 from dotenv import load_dotenv
 
+from script.db import initialize_db, init_db as create_tables_if_not_exist, engine as db_engine
+
+DATABASE_URL = f"sqlite+aiosqlite:///{project_root}/bot.db"
+LOG_FILE_PATH = project_root / "logs" / "bot.log"
+
+initialize_db(DATABASE_URL)
+
+
+from handlers import start, help, status, vm_commands
+
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
