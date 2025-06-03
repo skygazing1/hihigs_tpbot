d5d83be483c69b6bf1f54f5329c04714d84fdef8
konuxxd <golfralf34@gmail.com>
2025-06-04 01:26:35 +0300

    Валидация отсутствия аргументов

--- handlers/vm_commands.py
+++ handlers/vm_commands.py
@@ -17,3 +17,13 @@ vm_config_manager = VMConfigManager()
 async def vmpath_handler(message: Message):
     user_id = message.from_user.id
     args_text = message.text.split(maxsplit=1)[1] if len(message.text.split(maxsplit=1)) > 1 else ""
+
+    if not args_text:
+        await message.answer(
+            "Пожалуйста, укажите данные для подключения к ВМ.\n"
+            "Формат: <code>/vmpath &lt;host&gt; &lt;username&gt; &lt;password&gt;</code>\n"
+            "Или: <code>/vmpath &lt;host:port&gt; &lt;username&gt; &lt;password&gt;</code>\n"
+            "Пример: <code>/vmpath 192.168.1.100 myuser mypass123</code>\n"
+            "Пример: <code>/vmpath your.server.com:2222 myuser mypass123</code>"
+        )
+        return
